6d1e1bc1df8559a1ace76337a7d0aba14714e257
david.ding <davidinsuomi@gmail.com>
2024-06-05 00:13:47 +0800

    add pubkeyHasher & emailDomainHash

--- packages/tools/src/emailProof.ts
+++ packages/tools/src/emailProof.ts
@@ -1,10 +1,10 @@
-import { readFileSync, existsSync, mkdirSync, rm, writeFileSync } from 'fs';
-import { join } from 'path';
+import { readFileSync, existsSync, mkdirSync, rm, writeFileSync } from "fs";
+import { join } from "path";
 import { generateEmailVerifierInputs } from "@zk-email/helpers";
-import { Groth16Proof, PublicSignals, groth16 } from 'snarkjs';
+import { Groth16Proof, PublicSignals, groth16 } from "snarkjs";
 import { packBytesIntoNBytes } from "@zk-email/helpers";
 import { poseidon } from "@iden3/js-crypto";
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
 export type IEmailApproverCircuitInputs = {
     email_header: string[];
@@ -18,12 +18,12 @@ export type IEmailApproverCircuitInputs = {
 };
 
 export interface IEmailProof {
-    proof: bigint[],
-    pubkeyHash: bigint,
-    senderDomainHash: bigint,
-    senderCommitment: bigint,
-    controlAddress: string,
-    approvedHash: string
+    proof: bigint[];
+    pubkeyHash: bigint;
+    senderDomainHash: bigint;
+    senderCommitment: bigint;
+    controlAddress: string;
+    approvedHash: string;
 }
 
 export class EmailProof {
@@ -49,13 +49,15 @@ export class EmailProof {
         file_vkey: string,
         rapidsnarkProverBin?: string
     ) {
-        this.vKey = JSON.parse(readFileSync(file_vkey).toString('utf-8'));
+        this.vKey = JSON.parse(readFileSync(file_vkey).toString("utf-8"));
         // check if file_wasm and file_zkey exist
         if (!file_wasm || !file_zkey) {
             throw new Error("The file_wasm and file_zkey are required");
         }
-        if (!file_wasm.endsWith('.wasm') || !file_zkey.endsWith('.zkey')) {
-            throw new Error("The file_wasm must be a .wasm file and the file_zkey must be a .zkey file");
+        if (!file_wasm.endsWith(".wasm") || !file_zkey.endsWith(".zkey")) {
+            throw new Error(
+                "The file_wasm must be a .wasm file and the file_zkey must be a .zkey file"
+            );
         }
         if (!existsSync(file_wasm) || !existsSync(file_zkey)) {
             throw new Error("The file_wasm and file_zkey must exist");
@@ -71,14 +73,13 @@ export class EmailProof {
         this._file_wasm = file_wasm;
         this._file_zkey = file_zkey;
 
-
-        this._tmpDir = join(__dirname, '..', ".tmp");
+        this._tmpDir = join(__dirname, "..", ".tmp");
         if (!existsSync(this._tmpDir)) {
             mkdirSync(this._tmpDir);
         }
 
         // check if generateWitness exists
-        this.generateWitnessJsDir = join(__dirname, '..', "generateWitness");
+        this.generateWitnessJsDir = join(__dirname, "..", "generateWitness");
         if (!existsSync(this.generateWitnessJsDir)) {
             throw new Error("The generateWitness directory does not exist");
         }
@@ -90,7 +91,6 @@ export class EmailProof {
         }
     }
 
-
     /**
      * generate email address commitment
      *
@@ -100,21 +100,23 @@ export class EmailProof {
      * @return {*}  {bigint}
      * @memberof EmailProof
      */
-    public static emailAddrCommit(emailAddr: string, sender_email_commitment_rand: bigint): bigint {
+    public static emailAddrCommit(
+        emailAddr: string,
+        sender_email_commitment_rand: bigint
+    ): bigint {
         /*
-           template EmailAddrCommit(num_ints) {
-               signal input rand;
-               signal input email_addr_ints[num_ints];
-               signal output commit;
-
-               component poseidon = Poseidon(1+num_ints);
-               poseidon.inputs[0] <== rand;
-               for(var i=0; i<num_ints; i++) {
-                   poseidon.inputs[1+i] <== email_addr_ints[i];
-               }
-               commit <== poseidon.out;
-           }
-       */
+                           template EmailAddrCommit(num_ints) {
+                               signal input rand;
+                               signal input email_addr_ints[num_ints];
+                               signal output commit;
+                               component poseidon = Poseidon(1+num_ints);
+                               poseidon.inputs[0] <== rand;
+                               for(var i=0; i<num_ints; i++) {
+                                   poseidon.inputs[1+i] <== email_addr_ints[i];
+                               }
+                               commit <== poseidon.out;
+                           }
+                       */
 
         // @zk-email/circuits/utils/constants.circom
         const EMAIL_ADDR_MAX_BYTES = 256;
@@ -122,11 +124,17 @@ export class EmailProof {
         if (emailAddr.length > EMAIL_ADDR_MAX_BYTES) {
             throw new Error("Email address is too long");
         }
-        const _sender_email_addr_ints = packBytesIntoNBytes(emailAddr, MAX_BYTES_IN_FIELD);
+        const _sender_email_addr_ints = packBytesIntoNBytes(
+            emailAddr,
+            MAX_BYTES_IN_FIELD
+        );
         const sender_email_addr_ints: bigint[] = [];
         for (
             let index = 0;
-            index < (Math.ceil(EMAIL_ADDR_MAX_BYTES / MAX_BYTES_IN_FIELD) /*computeIntChunkLength()*/);
+            index <
+            Math.ceil(
+                EMAIL_ADDR_MAX_BYTES / MAX_BYTES_IN_FIELD
+            ) /*computeIntChunkLength()*/;
             index++
         ) {
             if (index < _sender_email_addr_ints.length) {
@@ -135,11 +143,93 @@ export class EmailProof {
                 sender_email_addr_ints[index] = BigInt(0);
             }
         }
-        const poseidonInputs = [sender_email_commitment_rand, ...sender_email_addr_ints];
+        const poseidonInputs = [
+            sender_email_commitment_rand,
+            ...sender_email_addr_ints,
+        ];
         const out = poseidon.hash(poseidonInputs);
         return out;
     }
 
+    public static emailDomainHash(emailDomain: string): bigint {
+        const DOMAIN_MAX_BYTES = 255;
+        const MAX_BYTES_IN_FIELD = 31;
+        const _email_domain_ints = packBytesIntoNBytes(
+            emailDomain,
+            MAX_BYTES_IN_FIELD
+        );
+        const email_domain_ints: bigint[] = [];
+        for (
+            let index = 0;
+            index <
+            Math.ceil(
+                DOMAIN_MAX_BYTES / MAX_BYTES_IN_FIELD
+            ) /*computeIntChunkLength()*/;
+            index++
+        ) {
+            if (index < _email_domain_ints.length) {
+                email_domain_ints[index] = BigInt(_email_domain_ints[index]);
+            } else {
+                email_domain_ints[index] = BigInt(0);
+            }
+        }
+        const out = poseidon.hash(email_domain_ints);
+        return out;
+    }
+
+    public static async dkimPublicKeyHash(rawEmail: string): Promise<any | null> {
+        /*
+                     https://zkrepl.dev/?gist=43ce7dce2466c63812f6efec5b13aa73
+                     pragma circom 2.1.6;
+                     include "circomlib/poseidon.circom";
+                     template PubkeyHasher(n, k) {
+                         signal input pubkey[k];
+                         signal output pubkey_hash;
+                         var k2_chunked_size = k >> 1;
+                         if(k % 2 == 1) {
+                             k2_chunked_size += 1;
+                         }
+                         log("k2_chunked_size", k2_chunked_size);
+                         signal pubkey_hash_input[k2_chunked_size];
+                         for(var i = 0; i < k2_chunked_size; i++) {
+                             if(i==k2_chunked_size-1 && k2_chunked_size % 2 == 1) {
+                                 log(i);
+                                 pubkey_hash_input[i] <== pubkey[2*i];
+                             } else {
+                                 log(i);
+                                 pubkey_hash_input[i] <== pubkey[2*i] + (1<<n) * pubkey[2*i+1];
+                             }
+                         }
+                         pubkey_hash <== Poseidon(k2_chunked_size)(pubkey_hash_input);
+                     }
+                     component main = PubkeyHasher(121, 17);
+                     */
+        const inputs = await generateEmailVerifierInputs(rawEmail);
+        const publicKey = inputs.pubkey;
+        let publicKeyArr = publicKey.map(BigInt);
+
+        const out = await this.pubkeyHasher(121, 17, publicKeyArr);
+        return out;
+    }
+
+    public static async pubkeyHasher(n: number, k: number, pubkey: bigint[]) {
+        let k2_chunked_size = Math.floor(k / 2);
+        if (k % 2 === 1) {
+            k2_chunked_size += 1;
+        }
+        let pubkey_hash_input = new Array(k2_chunked_size);
+        for (let i = 0; i < k2_chunked_size; i++) {
+            if (i === k2_chunked_size - 1 && k2_chunked_size % 2 === 1) {
+                pubkey_hash_input[i] = BigInt(pubkey[2 * i]);
+            } else {
+                pubkey_hash_input[i] =
+                    BigInt(pubkey[2 * i]) +
+                    (BigInt(1) << BigInt(n)) * BigInt(pubkey[2 * i + 1]);
+            }
+        }
+        const out = poseidon.hash(pubkey_hash_input);
+        return out;
+    }
 
     /**
      * generate proof from eml file path or eml content
@@ -149,11 +239,14 @@ export class EmailProof {
      * @return {*}  {(Promise<IEmailProof | null>)}
      * @memberof EmailProof
      */
-    public async proveFromEml(emlFilePathOrEmlContent: string, senderCommitmentRand: bigint): Promise<IEmailProof | null> {
+    public async proveFromEml(
+        emlFilePathOrEmlContent: string,
+        senderCommitmentRand: bigint
+    ): Promise<IEmailProof | null> {
         // check if the input is a file path or the content of the eml file
         let rawEmail: string;
-        if (emlFilePathOrEmlContent.endsWith('.eml')) {
-            rawEmail = readFileSync(emlFilePathOrEmlContent, 'utf8');
+        if (emlFilePathOrEmlContent.endsWith(".eml")) {
+            rawEmail = readFileSync(emlFilePathOrEmlContent, "utf8");
         } else {
             rawEmail = emlFilePathOrEmlContent;
         }
@@ -162,13 +255,21 @@ export class EmailProof {
         }
 
         const inputs = await generateEmailVerifierInputs(rawEmail);
-        const emailHeader = Buffer.from(inputs.emailHeader.map(c => Number(c)));
+        const emailHeader = Buffer.from(inputs.emailHeader.map((c) => Number(c)));
         const subjectPrefixBuffer = Buffer.from("subject:");
-        const subjectIndex = emailHeader.indexOf(subjectPrefixBuffer) + subjectPrefixBuffer.length;
+        const subjectIndex =
+            emailHeader.indexOf(subjectPrefixBuffer) + subjectPrefixBuffer.length;
         const headerSelectorBuffer = Buffer.from("\r\nfrom:");
-        const senderEmailNameEmailIndex = emailHeader.indexOf(headerSelectorBuffer) + headerSelectorBuffer.length;
-        const senderEmailIndex = emailHeader.subarray(senderEmailNameEmailIndex).indexOf(Buffer.from("<")) + senderEmailNameEmailIndex + 1;
-        const senderDomainIndex = emailHeader.subarray(senderEmailIndex).indexOf(Buffer.from("@")) + 1;
+        const senderEmailNameEmailIndex =
+            emailHeader.indexOf(headerSelectorBuffer) + headerSelectorBuffer.length;
+        const senderEmailIndex =
+            emailHeader
+                .subarray(senderEmailNameEmailIndex)
+                .indexOf(Buffer.from("<")) +
+            senderEmailNameEmailIndex +
+            1;
+        const senderDomainIndex =
+            emailHeader.subarray(senderEmailIndex).indexOf(Buffer.from("@")) + 1;
 
         const circuitinput: IEmailApproverCircuitInputs = {
             email_header: inputs.emailHeader,
@@ -178,7 +279,7 @@ export class EmailProof {
             sender_email_idx: senderEmailIndex.toString(),
             sender_email_commitment_rand: senderCommitmentRand.toString(),
             sender_domain_idx: senderDomainIndex.toString(),
-            subject_idx: subjectIndex.toString()
+            subject_idx: subjectIndex.toString(),
         };
 
         return this.proveFromCircuitinput(circuitinput);
@@ -191,9 +292,10 @@ export class EmailProof {
      * @return {*}  {(Promise<IEmailProof | null>)}
      * @memberof EmailProof
      */
-    public async proveFromCircuitinput(circuitinput: IEmailApproverCircuitInputs): Promise<IEmailProof | null> {
+    public async proveFromCircuitinput(
+        circuitinput: IEmailApproverCircuitInputs
+    ): Promise<IEmailProof | null> {
         try {
-
             const proveData = await this._fullProve(
                 circuitinput,
                 this._file_wasm,
@@ -208,7 +310,10 @@ export class EmailProof {
             if (res === true) {
                 // get contract inputs
                 const _controlAddress = BigInt(publicSignals[3]).toString(16);
-                const _approvedHash = ((BigInt(publicSignals[4]) << BigInt(128)) + BigInt(publicSignals[5])).toString(16);
+                const _approvedHash = (
+                    (BigInt(publicSignals[4]) << BigInt(128)) +
+                    BigInt(publicSignals[5])
+                ).toString(16);
                 return {
                     proof: [
                         BigInt(proof.pi_a[0]),
@@ -218,15 +323,16 @@ export class EmailProof {
                         BigInt(proof.pi_b[1][0]),
                         BigInt(proof.pi_b[1][1]),
                         BigInt(proof.pi_c[0]),
-                        BigInt(proof.pi_c[1])
+                        BigInt(proof.pi_c[1]),
                     ],
                     pubkeyHash: BigInt(publicSignals[0]),
                     senderDomainHash: BigInt(publicSignals[1]),
                     senderCommitment: BigInt(publicSignals[2]),
-                    controlAddress: '0x' + '0'.repeat(40 - _controlAddress.length) + _controlAddress,
-                    approvedHash: '0x' + '0'.repeat(64 - _approvedHash.length) + _approvedHash
+                    controlAddress:
+                        "0x" + "0".repeat(40 - _controlAddress.length) + _controlAddress,
+                    approvedHash:
+                        "0x" + "0".repeat(64 - _approvedHash.length) + _approvedHash,
                 };
-
             } else {
                 console.log("Invalid proof");
                 return null;
@@ -239,7 +345,13 @@ export class EmailProof {
 
     private _randomSeed = 0;
     private _randomString(): string {
-        return (this._randomSeed++) + "" + new Date().getTime() + "" + Math.floor(Math.random() * 100000);
+        return (
+            this._randomSeed++ +
+            "" +
+            new Date().getTime() +
+            "" +
+            Math.floor(Math.random() * 100000)
+        );
     }
 
     // DO NOT USE execAsync, it will cause the process to hang
@@ -259,12 +371,10 @@ export class EmailProof {
         circuitinput: IEmailApproverCircuitInputs,
         wasmFile: string,
         zkeyFile: string
-    ): Promise<
-        {
-            proof: Groth16Proof,
-            publicSignals: PublicSignals
-        } | null
-    > {
+    ): Promise<{
+        proof: Groth16Proof;
+        publicSignals: PublicSignals;
+    } | null> {
         // get random string
         const randomString = this._randomString();
         const tmpDir = join(this._tmpDir, randomString);
@@ -278,7 +388,12 @@ export class EmailProof {
 
         // generate `witness.wtns`, node generate_witness.js EmailApprover.wasm input.json witness.wtns
         const witnessFile = join(tmpDir, `witness.wtns`);
-        await this._execPromise(`node ${join(this.generateWitnessJsDir, "generate_witness.js")} ${wasmFile} ${circuitInputFile} ${witnessFile}`);
+        await this._execPromise(
+            `node ${join(
+                this.generateWitnessJsDir,
+                "generate_witness.js"
+            )} ${wasmFile} ${circuitInputFile} ${witnessFile}`
+        );
         if (!existsSync(witnessFile)) {
             console.error("Failed to generate witness file");
             return null;
@@ -288,15 +403,19 @@ export class EmailProof {
             if (this._rapidsnarkProverBin) {
                 const proofFile = join(tmpDir, `proof.json`);
                 const publicFile = join(tmpDir, `public.json`);
-                await this._execPromise(`${this._rapidsnarkProverBin} ${zkeyFile} ${witnessFile} ${proofFile} ${publicFile}`);
+                await this._execPromise(
+                    `${this._rapidsnarkProverBin} ${zkeyFile} ${witnessFile} ${proofFile} ${publicFile}`
+                );
 
                 if (!existsSync(proofFile) || !existsSync(publicFile)) {
                     console.error("Failed to generate proof");
                     return null;
                 }
 
-                let _proof: any = JSON.parse(readFileSync(proofFile).toString('utf-8'));
-                let _publicSignals: string[] = JSON.parse(readFileSync(publicFile).toString('utf-8'));
+                let _proof: any = JSON.parse(readFileSync(proofFile).toString("utf-8"));
+                let _publicSignals: string[] = JSON.parse(
+                    readFileSync(publicFile).toString("utf-8")
+                );
                 if (!_proof || !_publicSignals) {
                     console.error("Failed to generate proof");
                     return null;
@@ -311,9 +430,8 @@ export class EmailProof {
 
                 return {
                     proof,
-                    publicSignals
+                    publicSignals,
                 };
-
             } else {
                 return await groth16.prove(zkeyFile, witnessFile);
             }
@@ -324,6 +442,5 @@ export class EmailProof {
             // remove the tmp directory
             rm(tmpDir, { recursive: true }, () => { });
         }
-
     }
 }
--- packages/tools/src/main.ts
+++ packages/tools/src/main.ts
@@ -1,13 +1,13 @@
-import { join } from 'path';
+import { join } from "path";
 import { EmailProof } from "./emailProof";
-
+import { readFileSync, existsSync, mkdirSync, rm, writeFileSync } from "fs";
 
 async function main() {
     // get parent directory on windows and linux
-    const _parentDir = join(__dirname, '..');
-    const _file_wasm = join(_parentDir, "zkpFiles", 'EmailApprover.wasm');
-    const _file_zkey = join(_parentDir, "zkpFiles", 'emailapprover_final.zkey');
-    const _file_vkey = join(_parentDir, "zkpFiles", 'verification_key.json');
+    const _parentDir = join(__dirname, "..");
+    const _file_wasm = join(_parentDir, "zkpFiles", "EmailApprover.wasm");
+    const _file_zkey = join(_parentDir, "zkpFiles", "emailapprover_final.zkey");
+    const _file_vkey = join(_parentDir, "zkpFiles", "verification_key.json");
 
     // to check hanging
     // setInterval(() => {
@@ -17,35 +17,74 @@ async function main() {
     //#region EmailAddrCommit test
     {
         /*
-            input 1: 12322
-            input 2: xurigong@gmail.com
-            output: 21770830330223450464430503989801104958781861536559456253001293349309810700942
-        */
+                    input 1: 12322
+                    input 2: xurigong@gmail.com
+                    output: 21770830330223450464430503989801104958781861536559456253001293349309810700942
+                */
         const emailAddr = "xurigong@gmail.com";
         const commitment_rand = BigInt(12322);
-        const addressCommit = EmailProof.emailAddrCommit(emailAddr, commitment_rand); // static method
-        if (addressCommit !== BigInt("21770830330223450464430503989801104958781861536559456253001293349309810700942")) {
+        const addressCommit = EmailProof.emailAddrCommit(
+            emailAddr,
+            commitment_rand
+        ); // static method
+        if (
+            addressCommit !==
+            BigInt(
+                "21770830330223450464430503989801104958781861536559456253001293349309810700942"
+            )
+        ) {
             throw new Error("EmailAddrCommit test failed");
         }
+        const emailDomainHash = EmailProof.emailDomainHash("gmail.com");
+        if (
+            emailDomainHash !==
+            BigInt(
+                "19361475216702037345099198859178566376129780752330640287510661993072372998404"
+            )
+        ) {
+            throw new Error("email domain test failed");
+        }
+        let rawEmail: string;
+        rawEmail = readFileSync(join(_parentDir, "emls", "example2.eml"), "utf8");
+
+        const dkimPublicKey = await EmailProof.dkimPublicKeyHash(rawEmail);
+        console.log("dkimPublicKey:", dkimPublicKey);
     }
     //#endregion
 
-    const ParallelCount = 10;
+    const ParallelCount = 1;
     let rapidsnarkProverBin: string | undefined = undefined;
     // download from https://github.com/iden3/rapidsnark/releases/
-    rapidsnarkProverBin = "/usr/local/rapidsnark-macOS-arm64-v0.0.2/bin/prover";
+    console.log("PROVER_PATH:", process.env.PROVER_PATH);
+    rapidsnarkProverBin =
+        process.env.PROVER_PATH ||
+        "/usr/local/rapidsnark-macOS-arm64-v0.0.2/bin/prover";
 
-
-    const emailProof = new EmailProof(_file_wasm, _file_zkey, _file_vkey, rapidsnarkProverBin);
+    const emailProof = new EmailProof(
+        _file_wasm,
+        _file_zkey,
+        _file_vkey,
+        rapidsnarkProverBin
+    );
     const emailProofTest = async () => {
         const ts1 = Date.now();
         const commitment_rand = BigInt(12322);
-        const proof = await emailProof.proveFromEml(join(_parentDir, "emls", "example2.eml"), commitment_rand);
+        const proof = await emailProof.proveFromEml(
+            join(_parentDir, "emls", "example2.eml"),
+            commitment_rand
+        );
         if (proof === null) {
             throw new Error("EmailProof test failed");
         }
-        console.log("generate proof time:", Date.now() - ts1 + 'ms. via ' + (rapidsnarkProverBin === undefined ? 'snarkjs' : 'rapidsnark'));
-    }
+        console.log(proof);
+        console.log(
+            "generate proof time:",
+            Date.now() -
+            ts1 +
+            "ms. via " +
+            (rapidsnarkProverBin === undefined ? "snarkjs" : "rapidsnark")
+        );
+    };
 
     const arr = [];
     for (let index = 0; index < ParallelCount; index++) {
@@ -54,9 +93,14 @@ async function main() {
     console.log("start parallel test: " + ParallelCount + " times");
     const ts1 = Date.now();
     await Promise.all(arr);
-    console.log("total time:", Date.now() - ts1 + 'ms. via ' + (rapidsnarkProverBin === undefined ? 'snarkjs' : 'rapidsnark'));
-
-
+    console.log(
+        "total time:",
+        Date.now() -
+        ts1 +
+        "ms. via " +
+        (rapidsnarkProverBin === undefined ? "snarkjs" : "rapidsnark")
+    );
+    process.exit(0);
 }
 
 main();
